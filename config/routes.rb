8a5275369520f48d7b298e33b3b1f5eeb70d00df
Gernene <wallie988@gmail.com>
2017-04-15 08:43:32 +0000

    Change navbar according to account type

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get    '/search',   to: 'posts#search'
+  get    '/myposts',  to: 'posts#myposts'
   get    '/newpost',  to: 'posts#new'
   post   '/newpost',  to: 'posts#create'
   get '/mycommitments', to: 'requests#commitments'
